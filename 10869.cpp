6329b57c18d36bb1774ea9260e54b3513bf9e266
jihun8861 <126228828+jihun8861@users.noreply.github.com>
2023-06-29 22:55:43 +0900

    Add files via upload

---
+++ 10869.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main() {
+	int A=0, B=0;
+	cin >> A >> B;
+	int sum = A + B;
+	int minus = A - B;
+	int multiplication = A * B;
+	int mo = A / B;
+	int na = A % B;
+	cout << sum << endl;
+	cout << minus << endl;
+	cout << multiplication << endl;
+	cout << mo << endl;
+	cout << na << endl;
+}
---
+++ 10950.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main() {
+	int A=0, B=0;
+	int sum = A + B;
+	int T;
+	cin >> T;
+	for (int i = 0; i < T; i++) {
+		cin >> A >> B;
+		cout << A + B << endl;
+	}
+
+}
