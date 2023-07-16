6e826f7d1b43e98063a42db75ed84157f57ddd07
jihun8861 <126228828+jihun8861@users.noreply.github.com>
2023-07-17 00:40:35 +0900

    Add files via upload

--- 스택, 큐, 보기, 정렬.cpp
+++ 스택, 큐, 보기, 정렬.cpp
@@ -26,7 +26,17 @@ public:
 		cout << endl;
 	}
 	void sortQ() {
-
+		int tmp;
+		for (int i = 0; i < index; i++) {
+			for (int j = i; j < index; j++) {
+				if (stack[i] > stack[j])
+				{
+					tmp = stack[i];
+					stack[i] = stack[j];
+					stack[j] = tmp;
+				}
+			}
+		}
 	}
 };
 
@@ -37,16 +47,32 @@ public:
 	Queue() { index = 0; }
 	void inqueue(int num) {
 		queue[index] = num;
-		index += 1;
+		index++;
 	}
 	void dequeue() {
-
+		for (int i = 0; i < index; i++) {
+			queue[i] = queue[i + 1];
+		}
+		index--;
 	}
 	void showQue() {
-
+		for (int i = 0; i < index; i++) {
+			cout << queue[i] << ' ';
+		}
+		cout << endl;
 	}
 	void sortQ() {
-
+		int tmp;
+		for (int i = 0; i < index; i++) {
+			for (int j = i; j < index; j++) {
+				if (queue[i] > queue[j])
+				{
+					tmp = queue[i];
+					queue[i] = queue[j];
+					queue[j] = tmp;
+				}
+			}
+		}
 	}
 };
 
@@ -54,5 +80,48 @@ int main() {
 	Stack s;
 	Queue q;
 	int num;
+	int choice;
+	while (true) {
+		cout << "1 Queue 2 Stack 3 보기 4 정렬 >> ";
+		cin >> choice;
+		if (choice == 1) {
+			cout << "Queue" << endl;
+			cout << "1 추가 2 빼기 >> ";
+			cin >> choice;
+
+			if (choice == 1) {
+				cout << "input: ";
+				cin >> choice;
+				q.inqueue(choice);
+			}
+			else if (choice == 2) {
+				cout << "out" << endl;
+				q.dequeue();
+			}
+		}
+		else if (choice == 2) {
+			cout << "Stack" << endl;
+			cout << "1 추가 2 빼기 >> ";
+			cin >> choice;
+			if (choice == 1) {
+				cout << "input: ";
+				cin >> choice;
+				s.push(choice);
+			}
+			else if (choice == 2) {
+				cout << "out" << endl;
+				s.pop();
+			}
+		}
+		else if (choice == 3) {
+			q.showQue();
+			s.showStack();
+		}
+		else if (choice == 4) {
+			cout << "정렬을 진행합니다." << endl;
+			q.sortQ();
+			s.showStack();
+		}
+	}
 
 }
